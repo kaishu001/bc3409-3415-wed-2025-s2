b61dfcde33dd535a589b0ec33b6653eefa67443a
kaishu001 <leekaixu@gmail.com>
2025-01-15 08:31:50 +0000

    1

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 from flask import render_template,request
+
+app = Flask(__name__) # confirm that application belongs to u
+
+@app.route("/") # always run the first line first 
+def index(): # creates a template
+    return(render_template("index.html"))
+
+if __name__ == "__main__": # reconfirm such that permissions are given
+    app.run()
